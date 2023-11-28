7c0d58b15b28ff8bdafe3240bdee667e131ec1fd
Eduardo Branco <112195384+eduabranco@users.noreply.github.com>
2023-11-28 14:45:12 -0300

    /* command pattern */

--- src/main/Main.java
+++ src/main/Main.java
@@ -56,3 +56,85 @@ public class  Main{
     }
 
 }
+/*
+import java.util.Scanner;
+
+// Interface for Commands
+interface Command {
+    void execute();
+}
+
+// Concrete Command classes
+class Option1Command implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Option 1: Perform action for Option 1");
+        // Add functionality for Option 1 here
+    }
+}
+
+class Option2Command implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Option 2: Perform action for Option 2");
+        // Add functionality for Option 2 here
+    }
+}
+
+class ExitCommand implements Command {
+    @Override
+    public void execute() {
+        System.out.println("Exiting the program. Goodbye!");
+        // Add any necessary cleanup or exit code here
+        System.exit(0);
+    }
+}
+
+// Invoker class
+class Menu {
+    private Command[] commands;
+
+    public Menu(Command[] commands) {
+        this.commands = commands;
+    }
+
+    public void displayMenu() {
+        System.out.println("Menu:");
+        for (int i = 0; i < commands.length; i++) {
+            System.out.println((i + 1) + ". " + commands[i].getClass().getSimpleName());
+        }
+    }
+
+    public void processChoice(int choice) {
+        if (choice >= 1 && choice <= commands.length) {
+            commands[choice - 1].execute();
+        } else {
+            System.out.println("Invalid choice. Please try again.");
+        }
+    }
+}
+
+public class TextUserInterfaceWithCommandPattern {
+    public static void main(String[] args) {
+        Command[] commands = {new Option1Command(), new Option2Command(), new ExitCommand()};
+        Menu menu = new Menu(commands);
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+
+        while (true) {
+            menu.displayMenu();
+            System.out.print("Enter your choice: ");
+
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
+                menu.processChoice(choice);
+            } else {
+                System.out.println("Please enter a valid option.");
+                scanner.nextLine(); // Consume invalid input
+            }
+        }
+    }
+}
+
+ */
